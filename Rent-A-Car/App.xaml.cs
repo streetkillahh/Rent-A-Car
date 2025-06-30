3d25423949d12eab79f27a7997d9ea5786d0d982
streetkillahh <streetkillahh@mail.ru>
2025-06-30 17:52:19 +0500

    Add project files.

---
+++ Rent-A-Car/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Rent_A_Car
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
